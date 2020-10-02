30561d2036148ab4ea2162fad856737fe5c8b999
kehinde obanla <kehinde.o.obanla@gmail.com>
2020-10-02 16:08:14 -0500

    added comments

--- assignments/assigment3/flaskAppy2.py
+++ assignments/assigment3/flaskAppy2.py
@@ -10,7 +10,7 @@ import os
 
 app = Flask(__name__)
 CORS(app)
-
+#gets earthquake data from a json file
 def get_earthquakeData():
     filedata = 'assignments/assigment3/data/1996_3.json'
     if os.path.isfile(filedata):
@@ -19,8 +19,10 @@ def get_earthquakeData():
     else:
         return jsonify({"Error":"1996_3 not there!!"}),404
     return json.loads(data)
-
+#loads earthquke data to rtree with 
+#a uniqueue id
 def loadAiportintoRtree(earthquakedata):
+     # creats rtree index
     earthquakeTree = rtree.index.Index()
     earthquakeUniqueid = {}
     id = 10
@@ -32,11 +34,12 @@ def loadAiportintoRtree(earthquakedata):
         earthquakeTree.insert(id,(float(lat),float(lon),float(lat),float(lon)))
         id+=1
     return earthquakeTree,earthquakeUniqueid
-
+#base route
 @app.route('/')
 def index():
     return 'This is the base route'
-
+#on click creates a bounding box
+# with lat and long
 @app.route('/click/')
 def click():
     lng, lat = request.args.get("lngLat",None).split(",")
@@ -45,25 +48,31 @@ def click():
     maxy = float(float(lat) + 0.002)
     miny = float(float(lat) - 0.002)
     left, bottom, right, top = (minx, miny, maxx, maxy)
-    earthquakedata =  get_earthquakeData()
-    idForJson=0
-    earthquakertree,rtreeid = loadAiportintoRtree(earthquakedata)
+    earthquakedata =  get_earthquakeData() # gets earthquake data
+    idForJson=0 # counter
+    #gets tree id and rtree filled with earthquake data
+    earthquakertree,rtreeid = loadAiportintoRtree(earthquakedata) 
     answer_Collection = {
         'type':'FeatureCollection',
         'feature':[]
     }
-     
+     #stores the id of the closest earthquakes
     nearest = list(earthquakertree.nearest((left,bottom,right,top),5))
     nearestlist = []
+    # for each id get all other properties from
+    #rtee and add it to a list
     for item in nearest:
         nearestlist.append({
             'type':'feature',
              'geometry':rtreeid[item]['geometry'],
              'properties':rtreeid[item]['properties']
         })
-   # print(nearestlist[0])
+   # add nearestlist to a dictionary
+   #to make it a geojson file
     answer_Collection['feature'] = nearestlist
     idForJson+=1
+    #returns geojson and a list of numbers 
+    #to be used as id in the frontend
     return jsonify([str(idForJson),answer_Collection])
 if __name__ == '__main__':
     app.run(host='localhost', port=8080)
